704261557ed43dfcb90ecf8f6ca505c6e987e7ee
Alexander Guschin <alexguschin.work@gmail.com>
2018-01-11 19:30:53 +0300

    Rename Yoga framework scheme

---
+++ YogaKit/Yoga/Yoga.h
@@ -0,0 +1,19 @@
+//
+//  Yoga.h
+//  Yoga
+//
+//  Created by Alexander Guschin on 11.01.2018.
+//  Copyright © 2018 Guido Marucci Blas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Yoga.
+FOUNDATION_EXPORT double YogaVersionNumber;
+
+//! Project version string for Yoga.
+FOUNDATION_EXPORT const unsigned char YogaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yoga/PublicHeader.h>
+
+
